2b353bd30134a255e7e0d3b484d3c1b07573df99
David <ddfontes17@gmail.com>
2016-03-17 02:04:07 +0000

    Final version

--- video.v
+++ video.v
@@ -3,8 +3,7 @@ module video(
 	input clk,
 	output reg Hsync, Vsync,
 	output reg [7:0] R, G, B,
-	output reg [6:0] HEX0, HEX1, HEX2, HEX3,
-	output [6:0] HEX4, HEX5,
+	output reg [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
 	input [3:0] KEYS,
 	input [3:0] FUNC
 );
@@ -33,17 +32,13 @@ parameter PADDLE_HEIGHT = 83, PADDLE_WIDTH = 12;
 parameter BALL_SIZE = 10;
 // End constants
 
-assign HEX5 = dec2bin(userPoints);
-assign HEX4 = dec2bin(computerPoints);
-
 // Hpos= 0.. 1688
 //Vpos= 0..1066 --> 11 bits;
 
 reg [10:0] Hpos, Vpos; // Current Drawing Position
 
 // Code Variables go here
-reg h_direction, v_direction;
-reg [1:0] hSpeed;
+reg h_direction;
 reg [2:0] vSpeed;
 reg [6:0] userPoints, computerPoints;
 reg [10:0] player1X, player1Y, player2X, player2Y, ballX, ballY;
@@ -58,22 +53,20 @@ begin
 	begin
 		Hpos <= 11'd0;
 		Vpos <= 11'd0;
-		player1X <= 30;
+		player1X <= H_RES - 42;
 		player1Y <= V_CENTER - PADDLE_HEIGHT / 2;
-		player2X <= H_RES - 42;
+		player2X <= 30;
 		player2Y <= V_CENTER - PADDLE_HEIGHT / 2;
 		ballX <= H_CENTER;
 		ballY <= V_CENTER;
 		h_direction <= 1'b0;
-		v_direction <= 1'b1;
-		hSpeed <= 2'd1;
 		vSpeed <= 4'd4;
 		HEX0 <= ~7'b0111111;
 		HEX1 <= ~7'b0111111;
-		HEX2 <= ~7'b0111111;
-		HEX3 <= ~7'b0111111;
-		//HEX4 <= ~7'b0111111;
-		//HEX5 <= ~7'b0111111;
+		HEX2 <= ~7'b1000000;
+		HEX3 <= ~7'b1000000;
+		HEX4 <= ~7'b0111111;
+		HEX5 <= ~7'b0111111;
 		playerHit <= 11'd0;
 		computerHit <= 11'd0;
 		userPoints <= 4'd0;
@@ -130,7 +123,7 @@ begin
 				B <= 8'hff;
 			end
 			
-			// Draw user paddle (left)
+			// Draw player1 paddle (right)
 			if (draw(player1X, player1Y, Hpos, Vpos, 1'b0))
 			begin
 				R <= 8'hff;
@@ -138,7 +131,7 @@ begin
 				B <= 8'hff;
 			end
 			
-			// Draw computer paddle (right)
+			// Draw computer/player2 paddle (left)
 			if (draw(player2X, player2Y, Hpos, Vpos, 1'b0))
 			begin
 				R <= 8'hff;
@@ -166,67 +159,80 @@ begin
 					if ((Vpos == V_LINE-1) || (Vpos == 3*V_LINE/4) || (Vpos == V_LINE/2) || (Vpos == V_LINE/4))
 					begin 
 						
-							// User paddle moviment
-							if (KEYS[0] == 0)
+							if ((Vpos == V_LINE-1) || (Vpos == V_LINE/2))
 							begin
-								if (player1Y + PADDLE_HEIGHT < V_RES) player1Y <= player1Y + 1'b1;
-							end
-							else if (KEYS[1] == 0)
-								if (player1Y > 11'd0) player1Y <= player1Y - 1'b1;
-							
-							// Computer paddle movement
-							if (ballY > player2Y + PADDLE_HEIGHT / 2 && player2Y + PADDLE_HEIGHT < V_RES) player2Y <= player2Y + 1;
-							else if (ballY < player2Y && player2Y > 0) player2Y <= player2Y - 1;
-							
-							playerHit = hit(player1X, player1Y, ballX, ballY);
-							computerHit = hit(player2X - PADDLE_WIDTH, player2Y, ballX, ballY);
-							
-							// Ball horizontal direction
-							if (playerHit > 0 || computerHit > 0)
-							begin
-								HEX1 = dec2bin(playerHit); // debug
-								HEX0 = dec2bin(computerHit); // debug
-							
-								h_direction = ~h_direction;
-								vSpeed = playerHit + computerHit;
-							end
-							
-							// Ball vertical direction
-							if (ballY + BALL_SIZE / 2 >= V_RES || ballY - BALL_SIZE / 2 <= 0)
-								if (v_direction == 1'b0) v_direction = 1'b1;
-								else v_direction = 1'b0;
-							
-							// Ball movement Y
-							if ((ballY + BALL_SIZE/2) + 100 <= V_RES + 100 && (ballY - BALL_SIZE/2) + 100 >= 0 + 100)
-								ballY = ballY + vSpeed - 4;
-							else
-							begin
-								case (vSpeed)
-									1: vSpeed = 7;
-									2: vSpeed = 6;
-									3: vSpeed = 5;
-									4: vSpeed = 4;
-									5: vSpeed = 3;
-									6: vSpeed = 2;
-									7: vSpeed = 1;
-									default: vSpeed = 4;
-								endcase
-								ballY = ballY + vSpeed - 4;
-							end
-							
-							// Ball movement X
-							if (ballX < H_RES && ballX > 0)
-								if (h_direction == 1'b0) ballX <= ballX - 1;
-								else ballX <= ballX + 1;
-							else
-							begin
-								if (h_direction == 1'b1) userPoints = userPoints + 1;
-								else computerPoints = computerPoints + 1;
-								ballX = H_CENTER;
-								ballY = V_CENTER;
-								h_direction = ~h_direction;
-							end
-							
+								// Player1 paddle movement
+								if (KEYS[0] == 0)
+								begin
+									if (player1Y + PADDLE_HEIGHT < V_RES) player1Y <= player1Y + 2'b10;
+								end
+								else if (KEYS[1] == 0)
+									if (player1Y + 2 > 11'd0) player1Y <= player1Y - 2'b10;
+								
+								// Computer paddle movement
+								if (FUNC[0] == 0)
+								begin
+									if (ballY > player2Y + PADDLE_HEIGHT / 2 && player2Y + PADDLE_HEIGHT < V_RES) player2Y <= player2Y + 2'b10;
+									else if (ballY < player2Y && player2Y > 0) player2Y <= player2Y - 2'b10;
+								end
+								else
+								begin
+									if (KEYS[2] == 0)
+									begin
+										if (player2Y + PADDLE_HEIGHT < V_RES) player2Y <= player2Y + 2'b10;
+									end
+									else if (KEYS[3] == 0)
+										if (player2Y + 2 > 11'd0) player2Y <= player2Y - 2'b10;
+								end
+								
+								playerHit = hit(player1X - PADDLE_WIDTH, player1Y, ballX, ballY);
+								computerHit = hit(player2X, player2Y, ballX, ballY);
+								
+								// Ball horizontal direction
+								if (playerHit > 0 || computerHit > 0)
+								begin
+									h_direction = ~h_direction;
+									vSpeed = playerHit + computerHit;
+									if (h_direction == 1'b0) ballX = ballX - calculateSpeedX(vSpeed);
+									else ballX = ballX + calculateSpeedX(vSpeed);
+								end
+								
+								// Ball movement Y
+								if ((ballY + BALL_SIZE/2) + 100 <= V_RES + 100 && (ballY - BALL_SIZE/2) + 100 >= 0 + 100)
+									ballY = ballY + vSpeed - 4;
+								else
+								begin
+									case (vSpeed)
+										1: vSpeed = 7;
+										2: vSpeed = 6;
+										3: vSpeed = 5;
+										4: vSpeed = 4;
+										5: vSpeed = 3;
+										6: vSpeed = 2;
+										7: vSpeed = 1;
+										default: vSpeed = 4;
+									endcase
+									ballY = ballY + vSpeed - 4;
+								end
+								
+								// Ball movement X
+								if (ballX + 100 < H_RES + 100 && ballX + 100 > 0 + 100)
+									if (h_direction == 1'b0) ballX = ballX - calculateSpeedX(vSpeed);
+									else ballX = ballX + calculateSpeedX(vSpeed);
+								else
+								begin
+									if (h_direction == 1'b1) userPoints = userPoints + 1;
+									else computerPoints = computerPoints + 1;
+									ballX = H_CENTER;
+									ballY = V_CENTER;
+									h_direction = ~h_direction;
+								end
+								
+								HEX5 <= dec2bin(userPoints / 10);
+								HEX4 <= dec2bin(userPoints % 10);
+								HEX1 <= dec2bin(computerPoints / 10);
+								HEX0 <= dec2bin(computerPoints % 10);
+							end	
 					end // Vpos == 
 				end
 			end
@@ -281,7 +287,7 @@ input [10:0] paddleX, paddleY, ballX, ballY;
 
 integer result = 0;
 
-if (ballX == paddleX + PADDLE_WIDTH)
+if (ballX >= paddleX + PADDLE_WIDTH - 1 && ballX <= paddleX + PADDLE_WIDTH + 3)
 begin
 	if (ballY - BALL_SIZE/2 <= paddleY + PADDLE_HEIGHT && ballY + BALL_SIZE/2 >= paddleY)
 	begin
@@ -293,23 +299,46 @@ begin
 		hit = result;
 	end
 end
-else hit = 3'd0;
+else hit = 0;
 
 endfunction
 
-function integer calculateSpeed;
+/*
+Calculate X speed
+*/
+function integer calculateSpeedX;
 
 input [2:0] speed;
 
 case (speed)
-	1: calculateSpeed = 7;
-	2: calculateSpeed = 6;
-	3: calculateSpeed = 5;
-	4: calculateSpeed = 4;
-	5: calculateSpeed = 3;
-	6: calculateSpeed = 2;
-	7: calculateSpeed = 1;
-	default: calculateSpeed = 4;
+	1: calculateSpeedX = 2;
+	2: calculateSpeedX = 3;
+	3: calculateSpeedX = 4;
+	4: calculateSpeedX = 5;
+	5: calculateSpeedX = 4;
+	6: calculateSpeedX = 3;
+	7: calculateSpeedX = 2;
+	default: calculateSpeedX = 4;
+endcase
+
+endfunction
+
+/*
+Calculate Y speed
+*/
+function integer calculateSpeedY;
+
+input [2:0] speed;
+
+case (speed)
+	1: calculateSpeedY = 7;
+	2: calculateSpeedY = 6;
+	3: calculateSpeedY = 5;
+	4: calculateSpeedY = 4;
+	5: calculateSpeedY = 3;
+	6: calculateSpeedY = 2;
+	7: calculateSpeedY = 1;
+	default: calculateSpeedY = 4;
 endcase
 
 endfunction
